bfb7e894e6f52bc4958498050b4f02496ae165b0
Кирилл Щелухин <71601556+Kir-Scheluh@users.noreply.github.com>
2023-10-20 13:57:31 +0300

    a prototype of the menu has been made

---
+++ main.py
@@ -0,0 +1,16 @@
+while True:
+    current_command = input("Главное меню: \n"
+          "1 - анализ \n"
+          "2 - просмотр \n"
+          "3 - выход \n")
+    print(current_command)
+    if current_command == "1":
+        pass
+        #Функция для анализа
+    elif current_command == "2":
+        pass
+        #Функция для просмотра
+    elif current_command == "3":
+        break
+    else:
+        print("Неизвестная команда")
