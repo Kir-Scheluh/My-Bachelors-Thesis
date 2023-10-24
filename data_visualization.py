b64338673ae4f17421912bcfe3e62eeca4f33b40
Кирилл Щелухин <71601556+Kir-Scheluh@users.noreply.github.com>
2023-10-24 14:00:05 +0300

    the histogram plotting function has been tested and implemented

--- data_visualization.py
+++ data_visualization.py
@@ -5,6 +5,3 @@ def horizontal_histogram(data, names):
     fig = tpl.figure()
     fig.barh(data, names, force_ascii=False)
     fig.show()
-
-
-horizontal_histogram([15, 23, 8], ["Yahoo", "Ask", "Startpage"])
--- search.py
+++ search.py
@@ -2,12 +2,18 @@ from search_engines import Yahoo  # Работает
 from search_engines import Startpage  # Работает
 from search_engines import Ask  # Работает
 from filter_search_results import filter_results
+from data_visualization import horizontal_histogram
 
 
 def general_search(query):
     result = {'Yahoo': filter_results(search_with_yahoo(str(query))),
               'Startpage': filter_results(search_with_startpage(str(query))),
               'Ask': filter_results(search_with_Ask(str(query)))}
+    names = list(result.keys())
+    data = []
+    for i in result.values():
+        data.append(len(i["hosts"]))
+    horizontal_histogram(data, names)
 
     return result
 
@@ -34,4 +40,4 @@ def search_with_Ask(query):
     return output
 
 
-print(general_search('Влад бумага ютуб'))
+general_search('Влад бумага ютуб')
