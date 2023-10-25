2a8c783c1e09fe7e4be43f524736ae6b761a7fd7
Кирилл Щелухин <71601556+Kir-Scheluh@users.noreply.github.com>
2023-10-25 13:33:24 +0300

    the function that extracts the document title by URL is embedded in the search function

--- search.py
+++ search.py
@@ -1,18 +1,25 @@
-from search_engines import Yahoo  # Работает
-from search_engines import Startpage  # Работает
-from search_engines import Ask  # Работает
+from search_engines import Yahoo
+from search_engines import Startpage
+from search_engines import Ask
 from filter_search_results import filter_results
 from data_visualization import horizontal_histogram
+from get_title import get_html_title
 
 
-def general_search(query):
+def general_search(url):
+    query = get_html_title(url)
+    print(f'Запрос: {query}')
+
     result = {'Yahoo': filter_results(search_with_yahoo(str(query))),
               'Startpage': filter_results(search_with_startpage(str(query))),
               'Ask': filter_results(search_with_Ask(str(query)))}
+
     names = list(result.keys())
     data = []
     for i in result.values():
         data.append(len(i["hosts"]))
+
+    print('Количество результатов:')
     horizontal_histogram(data, names)
 
     return result
@@ -30,6 +37,7 @@ def search_with_startpage(query):
     engine = Startpage()
     search_result = engine.search(str(query))
     output = {'hosts': search_result.hosts(), 'titles': search_result.titles(), 'links': search_result.links()}
+
     return output
 
 
@@ -37,7 +45,8 @@ def search_with_Ask(query):
     engine = Ask()
     search_result = engine.search(str(query))
     output = {'hosts': search_result.hosts(), 'titles': search_result.titles(), 'links': search_result.links()}
+
     return output
 
 
-general_search('Влад бумага ютуб')
+general_search('')
