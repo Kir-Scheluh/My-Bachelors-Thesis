af03f6ea7a33dd7bd1fd67fa7174e486fea50737
Кирилл Щелухин <71601556+Kir-Scheluh@users.noreply.github.com>
2023-12-23 15:13:30 +0300

    Update main.py
    the "if else" construct has been replaced with the "match case" construct

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ while True:
                             "2 - просмотр \n"
                             "3 - выход \n")
     print(current_command)
-    if current_command == "1":
-        url = input("Введите url для запроса: ")
-        query = get_html_title(url)
-        general_search(query)
-    elif current_command == "2":
-        pass
-        # Функция для просмотра
-    elif current_command == "3":
-        break
-    else:
-        print(f"Неизвестная команда: '{current_command}'")
+    match current_command:
+        case "1":
+            url = input("Введите url для запроса: ")
+            query = get_html_title(url)
+            general_search(query)
+        case "2":
+            pass
+            # Функция для просмотра
+        case "3":
+            break
+        case _:
+            print(f"Неизвестная команда: '{current_command}'")
