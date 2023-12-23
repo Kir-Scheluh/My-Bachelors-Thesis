6dc21e235b915ceb9bdc72ed501293763d930fd2
Кирилл Щелухин <71601556+Kir-Scheluh@users.noreply.github.com>
2023-12-23 14:41:22 +0300

    Update filter_search_results.py

--- filter_search_results.py
+++ filter_search_results.py
@@ -4,11 +4,3 @@ from is_videohosting import is_video_hosting
 def filter_results(input_list):
     new_list = [x for x in input_list if not is_video_hosting(x)]
     return new_list
-# def filter_results(input_):
-#     new_dict = {'hosts': [], 'titles': [], 'links': []}
-#     for i in range(len(input_dict['hosts'])):
-#         if not is_video_hosting(input_dict['hosts'][i]):
-#             new_dict['hosts'].append(input_dict['hosts'][i])
-#             new_dict['titles'].append(input_dict['titles'][i])
-#             new_dict['links'].append(input_dict['links'][i])
-#     return new_dict
