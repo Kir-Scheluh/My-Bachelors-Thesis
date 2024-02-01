7e71df4e9243a149838b40ef9b211a1ad6a052b5
Кирилл Щелухин <71601556+Kir-Scheluh@users.noreply.github.com>
2024-02-01 15:06:46 +0300

    the filtering of results has been changed

--- filter_search_results.py
+++ filter_search_results.py
@@ -1,6 +1,32 @@
-from is_videohosting import is_video_hosting
-
+import re
 
 def filter_results(input_list):
     new_list = [x for x in input_list if not is_video_hosting(x)]
     return new_list
+
+def is_video_hosting(url):
+    return is_youtube_url(url) or is_vimeo_url(url) or is_rutube_url(url) or is_vk_url(url)
+
+
+def is_youtube_url(url):
+    pattern = r'(https?://)?(www\.)?youtube\.com(\w+)?'
+    match = re.search(pattern, url)
+    return match is not None
+
+
+def is_vimeo_url(url):
+    pattern = r'(https?://)?(www\.)?vimeo\.com(\w+)?'
+    match = re.search(pattern, url)
+    return match is not None
+
+
+def is_rutube_url(url):
+    pattern = r'(https?://)?(www\.)?rutube\.ru(\w+)?'
+    match = re.search(pattern, url)
+    return match is not None
+
+
+def is_vk_url(url):
+    pattern = r'(https?://)?(www\.)?vk\.com/video(\w+)?'
+    match = re.search(pattern, url)
+    return match is not None
