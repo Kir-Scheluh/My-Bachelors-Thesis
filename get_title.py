1b6fc55424c12ea29f40519f5f5408b5896650a9
Кирилл Щелухин <71601556+Kir-Scheluh@users.noreply.github.com>
2024-02-01 16:06:46 +0300

    blabla

--- get_title.py
+++ get_title.py
@@ -1,35 +1,12 @@
-# Импорты для обработки ошибок
-import socket
-import ssl
-import urllib3
+import re
 
-import requests
-from bs4 import BeautifulSoup
+def delete_url(original_string):
 
+    url_pattern = re.compile(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+')
+    match = url_pattern.search(original_string)
 
-def get_html_title(url):
-    try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        return soup.title.string.rstrip()
-    except requests.exceptions.Timeout:
-        print("Ошибка: превышено время ожидания")
-        return requests.exceptions.Timeout
-    except requests.exceptions.MissingSchema:
-        print("Неверный формат URL")
-        return requests.exceptions.MissingSchema
-    except AttributeError:
-        print("Ошибка при считывании заголовка страницы")
-        return AttributeError
-    except ssl.SSLCertVerificationError:
-        print("Ошибка: [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: certificate has expired")
-        return ssl.SSLCertVerificationError
-    except ssl.SSLError:
-        print("Ошибка: sslv3 alert handshake failure")
-        return ssl.SSLError
-    except socket.gaierror:
-        print("Ошибка: socket.gaierror: [Errno 11001] getaddrinfo failed")
-        return socket.gaierror
-    except urllib3.exceptions.NameResolutionError:
-        print("Ошибка: urllib3.exceptions.NameResolutionError")
-        return urllib3.exceptions.NameResolutionError
+    if match:
+        # если найдено совпадение, удалить URL адрес и все последующие символы
+        original_string = original_string.split(match.group())[0]
+
+    return (original_string)
--- search.py
+++ search.py
@@ -1,10 +1,10 @@
 # Импорты для работы программы
+import re
 from search_engines import Yahoo
 from search_engines import Google
 from search_engines import Ask
 from search_engines import Startpage
 from search_engines import Bing
-from filter_search_results import filter_results
 from data_visualization import horizontal_histogram
 
 
@@ -12,11 +12,11 @@ def general_search(query):
     if type(query) == Exception:
         return 0
 
-    result = {'Yahoo': filter_results(search_with_yahoo(str(query))),
-              'Google': filter_results(search_with_google(str(query))),
-              'Ask': filter_results(search_with_ask(str(query))),
-              'Startpage': filter_results(search_with_startpage(str(query))),
-              'Bing': filter_results(search_with_bing(str(query)))}
+    result = {'Yahoo': search_with(str(query), Yahoo),
+              'Google': search_with(str(query), Google),
+              'Ask': search_with(str(query), Ask),
+              'Startpage': search_with(str(query), Startpage),
+              'Bing': search_with(str(query), Bing)}
     names = list(result.keys())
     data = []
     for i in result.values():
@@ -28,41 +28,23 @@ def general_search(query):
     return result
 
 
-def search_with_yahoo(query):
-    engine = Yahoo()
+def search_with(query, engine):
+    engine = engine()
     search_result = engine.search(str(query))
-    output = search_result.links()
+    links = search_result.links()
+    titles = search_result.titles()
+    titles = [clear_url(title) for title in titles]
+    output = dict(zip(titles, links))
 
     return output
 
 
-def search_with_google(query):
-    engine = Google()
-    search_result = engine.search(str(query))
-    output = search_result.links()
+def clear_url(original_string):
+    url_pattern = re.compile(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+')
+    match = url_pattern.search(original_string)
 
-    return output
+    if match:
+        # если найдено совпадение, удалить URL адрес и все последующие символы
+        original_string = original_string.split(match.group())[0]
 
-
-def search_with_ask(query):
-    engine = Ask()
-    search_result = engine.search(str(query))
-    output = search_result.links()
-
-    return output
-
-
-def search_with_startpage(query):
-    engine = Startpage()
-    search_result = engine.search(str(query))
-    output = search_result.links()
-
-    return output
-
-
-def search_with_bing(query):
-    engine = Bing()
-    search_result = engine.search(str(query))
-    output = search_result.links()
-
-    return output
+    return original_string
