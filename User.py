38f5076bc2c402c04f64ecfd919b82323e9c01ae
Кирилл Щелухин <71601556+Kir-Scheluh@users.noreply.github.com>
2024-03-08 22:12:50 +0300

    added create_user method

--- User.py
+++ User.py
@@ -31,3 +31,17 @@ class User:
 
     def create_user(self, path_to_df="users_info.csv"):
         is_authorized, is_admin = self.check_login_info(path_to_df)
+        if not (is_authorized and is_admin):
+            return
+
+        login = input('Введите логин')
+        password = input('Введите пароль')
+        roots = input('Права администратора [true/false]')
+        ttl = input('Введите срок жизни учетной записи [%Y-%m-%d]')
+
+        fe.file_decrypt(path_to_df, self.key)
+        df = pd.read_csv(path_to_df)
+        df.loc[len(df.index)] = [login, password, roots, ttl]
+        df.to_csv(path_to_df)
+        fe.file_encrypt(path_to_df, self.key)
+
